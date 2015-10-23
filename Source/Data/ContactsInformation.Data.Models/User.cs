a4e05157ccf182aa4ab1dc6d058d98a8dc1ccfbd
Ivo Spasov <ivossss@yahoo.com>
2015-10-23 12:32:32 +0300

    User domain model added. DbContext class added. Connection string added to connect to SQL Server.

---
+++ Source/Data/ContactsInformation.Data.Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace ContactsInformation.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Sex { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string PhotoPath { get; set; }
+
+        public string Status { get; set; }
+    }
+}
---
+++ Source/Data/ContactsInformation.Data/ContactsInformationDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace ContactsInformation.Data
+{
+    using System.Data.Entity;
+
+    using ContactsInformation.Data.Models;
+
+    public class ContactsInformationDbContext : DbContext
+    {
+        public ContactsInformationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
