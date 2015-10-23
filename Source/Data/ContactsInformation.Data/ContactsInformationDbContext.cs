b357943de2619881a33f1c4f0a1eb1df81f7df0c
Ivo Spasov <ivossss@yahoo.com>
2015-10-23 12:47:19 +0300

    Code First migrations installed.

--- Source/Data/ContactsInformation.Data/ContactsInformationDbContext.cs
+++ Source/Data/ContactsInformation.Data/ContactsInformationDbContext.cs
@@ -3,12 +3,14 @@
     using System.Data.Entity;
 
     using ContactsInformation.Data.Models;
+    using ContactsInformation.Data.Migrations;
 
     public class ContactsInformationDbContext : DbContext
     {
         public ContactsInformationDbContext()
             : base("DefaultConnection")
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ContactsInformationDbContext, Configuration>());
         }
 
         public DbSet<User> Users { get; set; }
