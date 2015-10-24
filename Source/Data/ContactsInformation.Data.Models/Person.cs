584bec4911ed13c9c25db2a3c2e89d248a5bfbbe
Ivo Spasov <ivossss@yahoo.com>
2015-10-24 10:53:50 +0300

    Person table updated to have required length for firs and last name.

--- Source/Data/ContactsInformation.Data.Models/Person.cs
+++ Source/Data/ContactsInformation.Data.Models/Person.cs
@@ -9,15 +9,17 @@
         [Key]
         public int Id { get; set; }
 
+        [StringLength(50, MinimumLength = 3)]
         public string FirstName { get; set; }
 
+        [StringLength(50, MinimumLength = 3)]
         public string LastName { get; set; }
 
         public string Sex { get; set; }
 
         public string PhoneNumber { get; set; }
 
-        public string PhotoPath { get; set; }
+        public string PhotoUrl { get; set; }
 
         public string Status { get; set; }
     }
