3b6145edacd8c6c0d17681a7152984825085a832
Ivo Spasov <ivossss@yahoo.com>
2015-10-24 12:46:14 +0300

    peopleRepository added to home controller.

--- Source/Web/ContactsInformation.Web/Controllers/HomeController.cs
+++ Source/Web/ContactsInformation.Web/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace ContactsInformation.Web.Controllers
+﻿namespace ContactsInformation.Web.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using ContactsInformation.Data.Common.Repositories;
+    using ContactsInformation.Data.Models;
+
     public class HomeController : Controller
     {
+        private readonly IDeletableEntityRepository<Person> peopleRepository;
+
+        public HomeController(IDeletableEntityRepository<Person> peopleDeletableRepository)
+        {
+            this.peopleRepository = peopleDeletableRepository;
+        }
+
         public ActionResult Index()
         {
             return View();
