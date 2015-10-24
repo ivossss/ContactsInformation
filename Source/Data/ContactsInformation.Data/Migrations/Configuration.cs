93545e3f1eead6d8e72f5f64f1e8c81c094b70f2
Ivo Spasov <ivossss@yahoo.com>
2015-10-24 16:45:39 +0300

    Seed method updated to generate field with random length

--- Source/Data/ContactsInformation.Data/Migrations/Configuration.cs
+++ Source/Data/ContactsInformation.Data/Migrations/Configuration.cs
@@ -29,21 +29,22 @@ namespace ContactsInformation.Data.Migrations
             for (int i = 0; i < 20; i++)
             {
                 var newPerson = new Person();
-                newPerson.FirstName = this.GenerateRandomString(5, letters);
-                newPerson.LastName = this.GenerateRandomString(7, letters);
+                newPerson.FirstName = this.GenerateRandomString(3, 10, letters);
+                newPerson.LastName = this.GenerateRandomString(5, 10, letters);
                 newPerson.Sex = this.GetRandomNumber(2) == 1 ? "Male" : "Female";
-                newPerson.PhoneNumber = this.GenerateRandomString(9, numbers);
+                newPerson.PhoneNumber = this.GenerateRandomString(5, 9, numbers);
                 newPerson.Status = "Active";
                 context.People.Add(newPerson);
             }
         }
 
-        private string GenerateRandomString(int length, char[] allowedChars)
+        private string GenerateRandomString(int minLenght, int maxLenght, char[] allowedChars)
         {
+            int lenght = this.GetRandomNumber(minLenght, maxLenght);
             StringBuilder builder = new StringBuilder();
             int randomNumber;
 
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < lenght; i++)
             {
                 randomNumber = this.GetRandomNumber(allowedChars.Length);
                 builder.Append(allowedChars[randomNumber]);
@@ -55,6 +56,11 @@ namespace ContactsInformation.Data.Migrations
         private int GetRandomNumber(int maxValue)
         {            
             return this.random.Next(maxValue);
+        }
+
+        private int GetRandomNumber(int minValue, int maxValue)
+        {
+            return this.random.Next(minValue, maxValue);
         }
     }
 }
